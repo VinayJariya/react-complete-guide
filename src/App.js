d012999c2cfef8f93faf21ce36211b9f27c5570b
Vinay Jariya <vinayjariya@gmail.com>
2019-09-02 16:13:29 +0530

    Creeting Element using React Code

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1 className="App-title">Hi I am a React App !!!</h1>
+    //     <p className="App-intro">
+    //       This is a paragraph !!!
+    //     </p>
+    //   </div>
+    // );
+    return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi, I am a React App !!!'));
   }
 }
 
