2afc9a3e975931be47c693d023b91eac8f0f12a4
Vinay Jariya <vinayjariya@gmail.com>
2019-09-02 16:17:47 +0530

    Creating a component and using it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1 className="App-title">Hi I am a React App !!!</h1>
-    //     <p className="App-intro">
-    //       This is a paragraph !!!
-    //     </p>
-    //   </div>
-    // );
-    return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi, I am a React App !!!'));
+    return (
+      <div className="App">
+        <h1 className="App-title">Hi I am a React App !!!</h1>
+        <p className="App-intro">
+          This is a paragraph !!!
+        </p>
+        <Person/>
+        <Person/>
+        <Person/>
+      </div>
+    );
+    //return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi, I am a React App !!!'));
   }
 }
 
